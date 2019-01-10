e0913da4def05f1b1405ced00cc3bffbf9983d8f
Samuel Cornet <samuel@noukod.com>
2019-01-10 21:46:19 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'capybara/dsl'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-SINATRA_ACTIVESUPPORT_WARNING=false
+# SINATRA_ACTIVESUPPORT_WARNING=false
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"

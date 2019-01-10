3d4234afa1af2fd967cbe8f9ebd227e488995fa2
Samuel Cornet <samuel@noukod.com>
2019-01-10 20:36:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,5 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180314210852) do
-  create_table :users do |t|
-    t.string :username
-    t.string :password_digest
-  end
 
 end

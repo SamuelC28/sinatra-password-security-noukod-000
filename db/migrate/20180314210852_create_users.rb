ac9a29ea54ed11f0837162c35333631cca7065e7
Samuel Cornet <samuel@noukod.com>
2019-01-10 20:28:10 +0000

    Automatically backed up by Learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,9 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-      # create_table :users do |t|
-      #   t.string :username
-      #   t.string :password
-      #   t.decimal :balance
-      end
+  class CreateUsers < ActiveRecord::Migration[5.1]
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
     end
+  end
+ 
+  def down
+    drop_table :users
+  end
+end
 end
